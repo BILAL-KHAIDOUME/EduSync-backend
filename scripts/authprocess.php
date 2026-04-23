38b0ead6b1878a243657c136a5a95676651171b9
BILAL KHAIDOUME <bkhaidoume@gmail.com>
2026-04-23 09:53:00 +0100

    initialize project structure (public, scripts, includes)

---
+++ scripts/authprocess.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password_confirmation = $_POST['password_confirmation'];
+$signup_btn = $_POST['signup'];
+
+
+?>
